eb4271f871d5aa4aad511ed8f7c87f6550cf2e5d
IsIt2020 <isitsystem2020@gmail.com>
2020-12-03 21:15:50 +0900

    auth画面のスタイル読み込みを、ページのみに変更

--- resources/views/pages/auth/signup_confirm.blade.php
+++ resources/views/pages/auth/signup_confirm.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.mainLayout')
 
 @section('loadStyle')
-    <!-- components -->
-    <link rel="stylesheet" href="{{ asset('css/components/navigation.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/button.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/toggle-switch.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/input-field.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/components/info-button.css') }}">
     <link rel="stylesheet" href="{{ asset('css/pages/sign-up.css') }}">
 @endsection
 
