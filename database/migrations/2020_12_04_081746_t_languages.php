ce8a932cdf27d8b8a4b96ff04aacb3f99ddaf2fa
kazuki sannpe <kazuki.sannpe@gmail.com>
2020-12-04 17:25:19 +0900

    タグテーブル？、言語テーブルのmigration作成 ※この二つのテーブル特に想像できなくで適当になってしまっているので要確認、かたじけない

---
+++ database/migrations/2020_12_04_081746_t_languages.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TLanguages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_languages', function (Blueprint $table) {
+
+            // 言語ID(主キー)(※とりあえずsmallIncrementsにしてしまった)
+            $table->smallIncrements('language_id');
+            // 記事ID
+            $table->smallInteger('article_id');
+            // 記事の種類(なぜかlengthが1にならない助けて)
+            $table->tinyInteger('article_kind')->length(1);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_languages');
+    }
+}
