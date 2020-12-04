8f173ef20fc9fd307a70a2d5b361bd87ce0bacab
mis-o-ramen <satokazu123@gmail.com>
2020-12-04 10:40:44 +0900

    TOP画面　文字色修正 DoughnutChart.jsが古いままだったので更新

--- public/js/DoughnutChart.js
+++ public/js/DoughnutChart.js
@@ -1,36 +1,54 @@
-var colors = ['#007bff', '#28a745', '#333333', '#c3e6cb', '#dc3545', '#6c757d'];
 
+//チャート描画色
+var colors = ["#003d9f", "#0066d0", "#1b9744", "#59c871", "#ffd330", "#dda226", "#eb4435", "#b1010c"];
 //本番はこんな感じで
 //var data = @json($data);
-var data = [
-    ["Java", "PHP", "C#", "JavaScript"],
-    [38, 31, 21, 10],
-];
+var data = {
+    name: ["Java", "PHP", "C#", "JavaScript", "C", "Objective-C"],
+    value: [30, 25, 15, 13, 10, 7],
+};
 
+//#region チャート初期化
 function InitChart(data) {
     var chart = new Chart($('#chart'), {
         type: 'doughnut',
         data: {
-            labels: data[0],
+            labels: data.name,
             datasets: [{
                 backgroundColor: colors,
-                data: data[1],
+                data: data.value,
                 borderWidth: 0,
             }]
         },
         options: {
-            cutoutPercentage: 85,
+            responsive: true,
+            maintainAspectRatio: false,
+            cutoutPercentage: 80,
             legend: {
-                position: 'bottom',
-                labels: {
-                    pointStyle: 'square',
-                    usePointStyle: true
-                }
+                display: false,
             },
+            tooltips: {
+                enabled: false,
+            }
         },
 
     });
+
+    //#region Legend追加
+    for (i = 0; i < data.value.length; i++) {
+
+        var legend = `<div class="legend">
+                <div class="color-pan" style='background:` + colors[i] + `'></div>
+                <p class="text">` + data.name[i] + `</p>
+                <p class="percentage">` + data.value[i] + `</p>
+            </div>`;
+
+        $('#chart-legends').append(legend);
+    }
+    //#endregion
 }
+//#endregion
+
 $(function() {
     InitChart(data);
 });
