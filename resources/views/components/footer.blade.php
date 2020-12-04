e924e12fc03d8557b0b1465e96c9f9f75dad49fa
mis-o-ramen <satokazu123@gmail.com>
2020-12-04 12:42:12 +0900

    NavigationBarフォント修正

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,26 @@
 <!--Footer-->
-<div class="footer">
-        <div class="footer-block">
-            <h2>Page One</h2>
-            <a href="#">Page Link One</a>
-            <a href="#">Page Link Two</a>
-            <a href="#">Page Link Three</a>
-        </div>
-        <div class="footer-block">
-            <h2>Page Two</h2>
-            <a href="#">Page Link One</a>
-            <a href="#">Page Link Two</a>
-            <a href="#">Page Link Three</a>
-        </div>
-        <div class="footer-block">
-            <h2>Page Three</h2>
-            <a href="#">Page Link One</a>
-            <a href="#">Page Link Two</a>
-            <a href="#">Page Link Three</a>
-            <a href="#">Page Link Four</a>
-            <a href="#">Page Link Five</a>
-        </div>
-
-        <h2 class="footer-logo">IS IT</h2>
+<div class="footer font-decorated">
+    <div class="footer-block">
+        <h2>Page One</h2>
+        <a href="#">Page Link One</a>
+        <a href="#">Page Link Two</a>
+        <a href="#">Page Link Three</a>
     </div>
-    <!--Footer-->
+    <div class="footer-block">
+        <h2>Page Two</h2>
+        <a href="#">Page Link One</a>
+        <a href="#">Page Link Two</a>
+        <a href="#">Page Link Three</a>
+    </div>
+    <div class="footer-block">
+        <h2>Page Three</h2>
+        <a href="#">Page Link One</a>
+        <a href="#">Page Link Two</a>
+        <a href="#">Page Link Three</a>
+        <a href="#">Page Link Four</a>
+        <a href="#">Page Link Five</a>
+    </div>
+
+    <h2 class="footer-logo">IS IT</h2>
+</div>
+<!--Footer-->
