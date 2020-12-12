50e888f401cf82e2bef483aa491d31203dcd6267
s.mitsuoka <physics.sm0414@gmail.com>
2020-12-12 17:46:51 +0900

    定数ファイル作成

---
+++ config/const.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    'TEST_MESSAGE' => 'テスト中です。',
+    'ERROR_MESSAGE' => 'エラーが発生しました。',
+
+    'uploadFile' => [
+        'UPLOAD_IMAGE_PATH' => 'public/image/'
+    ],
+
+    'imageUplodModal' => [
+        'DISP_THIS_ARTICLE_IMAGE' => '0',
+        'DISP_ALL_IMAGE' => '1',
+    ]
+];
